845a18b6b32e708041936fab12016f4a581a0e00
Tahira-Hafeez <tahirahafeez285@gmail.com>
2026-03-07 20:56:32 +0500

    responsiveness

--- lab assignment 2/index.js
+++ lab assignment 2/index.js
@@ -1,1 +1,6 @@
-console.log("hello world!!!!!!!");
+const icon = document.getElementById("menubtn");
+const navbar = document.getElementById("navbar");
+
+icon.addEventListener("click", function() {
+    navbar.classList.toggle("show");
+});
