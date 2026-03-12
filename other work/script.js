dd39ce58782d9e2e548477ba991b44d77784fac6
Tahira-Hafeez <tahirahafeez285@gmail.com>
2026-03-12 22:35:05 +0500

    javascript

---
+++ other work/script.js
@@ -0,0 +1,10 @@
+// 2. function changeText() {
+//   document.getElementById("text").innerHTML = "Hello World";
+//   document.getElementById("text").style.color = "white";
+//  document.getElementById("text").style.background = "black";
+//  document.getElementById("text").style.fontSize = "20px";
+// document.getElementById("text").style.margin = "50px";
+        
+// }
+
+// 3. 
